aa6a4f5cb9a964f3240481c929d4c2c806185c18
fadeed007 <continuar007@gmail. com>
2021-06-10 16:17:54 -0400

    First commit

---
+++ src/help.js
@@ -0,0 +1,25 @@
+const help = (prefix) => {
+	return `
+-------------------( PRIVATE BOT DO 0 )---------------
+|•${prefix}sticker
+|•${prefix}attp
+|•${prefix}promote
+|•${prefix}demote
+|•${prefix}marcar
+|•${prefix}welcome [1/0]
+|•${prefix}del
+|•${prefix}hidetag
+|•${prefix}simi seu texto
+|•${prefix}em breve sai os videos mostrando
+|
+|•••BY SKILLER•••
+|
+|
+---------------------(PRIVATE BOT DO 0 )-----------------
+
+
+
+`
+}
+
+exports.help = help
